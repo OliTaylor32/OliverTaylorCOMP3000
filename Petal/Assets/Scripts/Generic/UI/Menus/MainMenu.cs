54521db9e12954fe39fc15eee1aa65dd30cbcb23
Oli <oli.taylor.2001@gmail.com>
2020-11-07 00:10:03 +0000

    Character model, Menus and Optimisation
    New base character model has been created
    
    Created a Main Menu (not linked up to the rest of the game currently)
    
    Started implementing batching and occlusion culling for game optimisation.

---
+++ Petal/Assets/Scripts/Generic/UI/Menus/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool cooldown;
+    public GameObject selector;
+    public int selected;
+    public Transform[] positions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown = false;
+        selected = 0;
+        selector.transform.position = new Vector3(selector.transform.position.x, selector.transform.position.y, positions[0].position.z);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selector.transform.position = new Vector3(selector.transform.position.x, selector.transform.position.y, positions[selected].position.z);
+
+        if (Input.GetAxis("Vertical") > 0.1f && cooldown == false)
+        {
+            if (selected != 0)
+            {
+                selected--;
+                StartCoroutine(CoolDown());
+            }
+        }
+
+        if (Input.GetAxis("Vertical") < -0.1f && cooldown == false)
+        {
+            if (selected != positions.Length - 1)
+            {
+                selected++;
+                StartCoroutine(CoolDown());
+            }
+        }
+    }
+
+    private IEnumerator CoolDown()
+    {
+        cooldown = true;
+        yield return new WaitForSeconds(0.2f);
+        cooldown = false;
+    }
+}
