2ff31c65650e541b9a103d5548ca233eb1bd7e88
Oli <oli.taylor.2001@gmail.com>
2020-12-14 03:07:48 +0000

    Work started on First Stage
    Added normal maps to clocktower base
    
    Created and added a generic straight for first stage

---
+++ Petal/Assets/Scripts/Generic/PlayerData/MainGameData.cs
@@ -0,0 +1,19 @@
+﻿
+public static class MainGameData
+{
+    public static int speedXP;
+    public static int attackXP;
+    public static int boostXP;
+    public static bool[] cleared = new bool[14];
+    public static int lastArea;
+    public static int speedLvl;
+    public static int attackLvl;
+    public static int boostLvl;
+    public static int gold;
+    public static int headwear;
+    public static int costume;
+    public static int gloves;
+    public static int shoes;
+    public static bool[] discovered = new bool[7];
+
+}
