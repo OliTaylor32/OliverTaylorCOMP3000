50b1ee84136a35398815995c743e5f532ed1b959
Oli <oli.taylor.2001@gmail.com>
2021-02-03 03:37:22 +0000

    Drawbridge
    The drawbridge has been created and is fully functional. Once the player completed the button puzzle, the drawbridge will open.

---
+++ Petal/Assets/Scripts/Level1/Drawbridge.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawbridge : MonoBehaviour
+{
+    private Animator anim;
+    public Button[] buttons;
+    private bool complete;
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        complete = false;
+        anim = GetComponent<Animator>();
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (complete == false)
+        {
+            bool passed = true;
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (buttons[i].down == false)
+                {
+                    passed = false;
+                    break;
+                }
+            }
+            if (passed == true)
+            {
+                anim.enabled = true;
+                complete = true;
+            }
+            if (Time.time - time > 0.1f && Time.time - time < 1f)
+            {
+                anim.enabled = false;
+            }
+        }
+        
+    }
+}
